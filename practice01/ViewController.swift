24e8bd7df4534ae1bff7a597f7496533db2cbccd
Your Name <you@example.com>
2019-03-17 17:51:01 +0800

    2019 / 3 / 17

--- practice01/ViewController.swift
+++ practice01/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func changeSlider(_ sender: UISlider) {
+        let font = inputText.font!
+        let newFont = font.withSize(CGFloat(sender.value))
+        inputText.font = newFont
+        labelText.font = newFont
+        fontSizeLabel.text = "字體大小： " + String(format: "%.2f", sender.value)
+    }
+    
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+//        print(sender.text!)
+        let inputTextCount = sender.text!.count
+//        print(inputTextCount)
+        labelText.text = "字數：\(inputTextCount)"
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+    }
+    @IBAction func viewTapped(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 }
 
